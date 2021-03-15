f779db75e5fbe74dd74d575bb12793afffacd1c4
Igor <igorhenriqueconstant@gmail.com>
2021-03-15 18:31:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+[![Generic badge](https://img.shields.io/badge/<EmDesenvolvimento>-<COLOR>.svg)](https://shields.io/)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+## Gulp Help
+
+Desenvolvi esse **"help"** do Gulp para facilitar na hora de automatizar algumas tarefas do Front-end.
+
+**ENG: ** I Developed this help from Gulp to make a easier tasks to automate front-end.
+
+
+------------
+
+#### Instalação
+Para realizar a instalção é necessário ter o NodeJS instalado, para assim poder fazer o download das respectivas dependências. 
+
+**- Você pode utilizar tanto o NPM quanto o YARN**
+
+```bash
+npm init
+yarn
+```
+
+------------
+
+#### Depedências Necessárias:
+:exclamation: npm install --global gulp-cli
+:exclamation: npm i gulp-sass
+:exclamation: npm install --save-dev gulp-connect-php
---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const connect = require('gulp-connect-php');
+const imagemin = require('gulp-imagemin');
+
+
+function minImage() {
+    return gulp
+        .src('assets/images')
+        .pipe(gulp.dest("Insira o caminho Destino das Imagens"))
+        .pipe(imagemin[
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    {
+                        removeViewBox: false,
+                        collapseGroups: true
+                    }
+                ]
+            })
+        ])
+
+}
+
+gulp.task('boradiminuir', minImage);
+
+
+// Em utilização de um projeto com PHP
+function connectPHP() {
+    connect.server({}, function () {
+        browserSync({
+            proxy: '127.0.0.1:8000'
+        });
+    })
+}
+
+gulp.task('boraconectar', connectPHP);
+
+
+/** Função para Compilar Arquivos .scss **/
+function compilarSass() {
+    return gulp
+        .src('styles/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            Browserlist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.stream());
+}
+
+gulp.task('sass', compilarSass);
+
+
+
+/** Função para Compilar Arquivos .js **/
+function gulpJS() {
+    return gulp.src([
+        //Insira aqui os Scripts que serão compilados.
+    ])
+
+        .pipe(concat('main.js'))
+        .pipe(uglify('main.js'))
+        .pipe(gulp.dest('assets/js'))
+        .pipe(browserSync.stream());
+}
+
+
+gulp.task('mainjs', gulpJS);
+
+
+/** Função para Espionar Alterações no Projeto **/
+function watchproject() {
+    gulp.watch('styles/*.scss', compilarSass);
+    gulp.watch('scripts/*.js', gulpJS);
+    gulp.watch('*.php', connectPHP).on('change', function () {
+        browserSync.reload();
+    });
+}
+
+gulp.task('watch', watchproject);
+
+
+
+//Em utilização de um projeto HTML
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+}
+
+//Tarefa para iniciar o Browser Sync
+gulp.task('browser-sync', browser);
+
+
+
+/** Criação da Tarefa =) That's All Folks! **/
+gulp.task('default', gulp.parallel('watch', 'sass', 'mainjs', 'browser-sync', 'boraconectar'))
