4f93a311ade894ef88fa109529d83830859d5c18
Igor <34916756+IgorConstant@users.noreply.github.com>
2025-05-13 12:42:04 -0300

    Melhorias e novos itens no Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,85 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
 
-// Compile Sass
+// Configurações
+const paths = {
+    styles: {
+        src: 'styles/**/*.scss',
+        dest: 'static/css'
+    },
+    scripts: {
+        src: 'scripts/**/*.js',
+        dest: 'static/js'
+    },
+    images: {
+        src: 'images/**/*',
+        dest: 'static/images'
+    },
+    html: './*.html'
+};
+
+// Compila SCSS para CSS, aplica autoprefixer, gera sourcemaps
 function styles() {
-    return gulp
-        .src('styles/main.scss')
-        .pipe(sass({
-            outputStyle: 'compressed',
-            quietDeps: true // Isso só remove warnings relacionados a dependências do Sass
-        }).on('error', sass.logError)) // Adicione para evitar quebra por erros no Sass
-        .pipe(gulp.dest('static/css'))
+    return gulp.src(paths.styles.src)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed', quietDeps: true }).on('error', sass.logError))
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.styles.dest))
         .pipe(browserSync.stream());
 }
 
-// Concatenate & Minify JS
+// Concatena e minifica arquivos JS
 function scripts() {
-    return gulp.src([
-        '', // Insira os arquivos de extensão JS aqui
-    ])
+    return gulp.src(paths.scripts.src)
+        .pipe(plumber())
         .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('static/js')) // Aqui você pode ajustar o diretorio que irá salvar o arquivo comprimido
+        .pipe(gulp.dest(paths.scripts.dest))
         .pipe(browserSync.stream());
 }
 
-// Watch files
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    gulp.watch('styles/*.scss', styles);
-    gulp.watch('scripts/*.js', scripts);
-    gulp.watch('*.html').on('change', browserSync.reload); // Recarrega quando HTML muda
+// Otimiza imagens
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest));
 }
 
-// Tasks
-gulp.task('styles', styles);
-gulp.task('scripts', scripts);
-gulp.task('watch', watch);
-gulp.task('default', gulp.parallel('styles', 'scripts', 'watch'));
+// Limpa os diretórios de build
+function clean() {
+    return del(['static/css', 'static/js', 'static/images']);
+}
+
+// Inicia servidor local e observa mudanças
+function watch() {
+    browserSync.init({
+        server: { baseDir: './' }
+    });
+
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.html).on('change', browserSync.reload);
+}
+
+// Exporta as tarefas
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.watch = watch;
+exports.default = gulp.series(
+    clean,
+    gulp.parallel(styles, scripts, images),
+    watch
+);
